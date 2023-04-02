d501e9a6a86b75b59178d59c4a97ad15fc6fd44c
pedro0g0m <pedrogmoreira94@gmail.com>
2023-04-02 21:02:37 +0100

    Fim da 8 lição

--- Section8OrganizingTheFirstMicrosservicesArchitecture/GeekShooping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
+++ Section8OrganizingTheFirstMicrosservicesArchitecture/GeekShooping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -27,7 +27,7 @@ namespace GeekShopping.ProductAPI.Repository
         {
             Product product = 
                 await _context.Products.Where(p => p.Id == id)
-                .FirstOrDefaultAsync();
+                .FirstOrDefaultAsync() ?? new Product();
             return _mapper.Map<ProductVO>(product);
         }
 
@@ -53,10 +53,11 @@ namespace GeekShopping.ProductAPI.Repository
             {
                 Product product =
                 await _context.Products.Where(p => p.Id == id)
-                    .FirstOrDefaultAsync();
-                if(product == null) return false;
+                    .FirstOrDefaultAsync() ?? new Product();
+                if (product.Id <= 0) return false;
                 _context.Products.Remove(product);
                 await _context.SaveChangesAsync();
+                return true;
             }
             catch (Exception)
             {
